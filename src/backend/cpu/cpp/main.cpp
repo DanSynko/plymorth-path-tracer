c216b529013607d79147f7d18ba16b6f0fcf3fb7
Данило <@LAPTOP-7M1VSVI1>
2026-08-02 18:44:07 +0300

    feat: implement prototype of path tracer
    - Add coordinate transformation from Raster Space to Camera Space.
    - Implement primary ray generation matching the camera viewport.
    - Add analytical ray-sphere intersection logic.
    - Implement basic RGB color structure with float precision.

--- src/backend/cpu/cpp/main.cpp
+++ src/backend/cpu/cpp/main.cpp
@@ -1,13 +1,74 @@
 ﻿#ifdef __INTELLISENSE__
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include <print>
+#include <numbers>
 #else
 import std;
 #endif
 
+import linear_algebra;
 import path_tracing_objects;
+
+constexpr float screen_width = 300.0f;
+constexpr float screen_height = 200.0f;
+constexpr float aspect_ratio = screen_width / screen_height;
+constexpr int FOV = 75;
+
+[[nodiscard]] std::pair<float, float> raster_to_camera(int x, int y) noexcept {
+	float x_normalized_device_coordinates = (x + 0.5) / screen_width;
+    float y_normalized_device_coordinates = (y + 0.5) / screen_height;
+
+    float x_screen_space = 2 * x_normalized_device_coordinates - 1;
+    float y_screen_space = 1 - 2 * y_normalized_device_coordinates;
+
+    float grad_to_rad = FOV * (std::numbers::pi / 180);
+    float FOV_scale_factor = std::tan(grad_to_rad / 2);
+
+    float x_cam = x_screen_space * aspect_ratio * FOV_scale_factor;
+    float y_cam = y_screen_space * FOV_scale_factor;
+
+    return {x_cam, y_cam};
+}
 
 int main() {
 	std::println("Hello, world!");
+
+    std::vector<plymorth::Color> frame_buffer(screen_width * screen_height);
+
+	plymorth::Vec3 camera{};
+	plymorth::Sphere sphere{};
+
+	for (int x = 0; x < screen_width; ++x) {
+        for (int y = 0; y < screen_height; ++y) {
+            auto [x_cam, y_cam] = raster_to_camera(x, y);
+
+            plymorth::Ray primary_ray = {
+				camera, 
+				{x_cam, y_cam, -1.0f}
+			};
+
+            primary_ray.direction = plymorth::normalize(primary_ray.direction);
+            plymorth::Color current_pixel_color = plymorth::hit_sphere(sphere, primary_ray);
+
+            frame_buffer[y * screen_width + x] = current_pixel_color;
+		}
+	}
+
+    std::ofstream out("image.ppm");
+
+    out << "P3\n" << screen_width << ' ' << screen_height << "\n255\n";
+    for (const auto& current_pixel_color : frame_buffer) {
+        out << current_pixel_color.r << " " 
+			<< current_pixel_color.g << " " 
+			<< current_pixel_color.b << "\n";
+    }
+
+	out.close();
+
+	std::println("Image created successfully!");
+
 	return 0;
 }
--- src/core/path_tracing_objects.cppm
+++ src/core/path_tracing_objects.cppm
@@ -2,6 +2,7 @@ export module path_tracing_objects;
 
 #ifdef __INTELLISENSE__
 #include <vector>
+#include <cmath>
 #else
 import std;
 #endif
@@ -14,8 +15,46 @@ struct Ray {
     Vec3 direction;
 };
 
-struct Sphere {
-    Vec3 position;
-    float radius;
+struct Color {
+    float r, g, b = 255.0f;
 };
+
+struct Sphere {
+    Color color = {255.0f, 0.0f, 0.0f};
+    Vec3 center = { 0.0f, 0.0f, -5.0f };
+    float radius = 1.0f;
+};
+
+[[nodiscard]] Color hit_sphere(Sphere sphere, Ray current_ray) noexcept {
+    float a = dot_product(current_ray.direction, current_ray.direction);
+
+    Vec3 V = current_ray.origin - sphere.center; 
+    float b = 2 * dot_product(current_ray.direction, V);
+
+    float c = dot_product(V, V) - std::powf(sphere.radius, 2);
+
+
+    float discriminant = (std::powf(b, 2)) - 4 * a * c;
+
+    if (discriminant < 0) {
+        return Color{ 0.0f, 0.0f, 255.0f };
+    }
+
+    float first_root = (-b - std::sqrt(discriminant)) / 2;
+    float second_root = (-b + std::sqrt(discriminant)) / 2;
+
+    float& nearest_root = first_root;
+
+    constexpr float t_min = 0.001f;
+    constexpr float t_max = std::numeric_limits<float>::infinity();
+
+    if (nearest_root < t_min || nearest_root > t_max) {
+        nearest_root = second_root;
+        if (nearest_root < t_min || nearest_root > t_max) {
+            return Color{0.0f, 0.0f, 255.0f};
+        }
+    }
+
+    return sphere.color;
+}
 } // namespace plymorth
